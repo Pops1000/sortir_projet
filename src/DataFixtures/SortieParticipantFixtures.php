9ba5ef8e890ac37e5641216dcfd53d72d98d637f
Pops1000 <peiothurin@gmail.com>
2023-10-23 10:50:48 +0200

    fix hashage mot de passe dans les fixtures

--- src/DataFixtures/SortieParticipantFixtures.php
+++ src/DataFixtures/SortieParticipantFixtures.php
@@ -10,9 +10,18 @@ use App\Entity\Sortie;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class SortieParticipantFixtures extends Fixture implements DependentFixtureInterface
 {
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $participants = [];
@@ -21,27 +30,35 @@ class SortieParticipantFixtures extends Fixture implements DependentFixtureInter
         $lieu = $manager->getRepository(Lieu::class)->findAll();
         for ($i = 1; $i <= 5; $i++) {
             $participant = new Participant();
+
             $participant->setMail("participant$i@example.com");
-            $participant->setNom("Nom $i");
-            $participant->setPrenom("Prenom $i");
-            $participant->setTelephone("Telephone $i");
-            $participant->setPseudo("Pseudo $i");
-            $participant->setMotPasse("mdp $i");
+            $participant->setNom("Nom$i");
+            $participant->setPrenom("Prenom$i");
+            $participant->setTelephone("Telephone$i");
+            $participant->setPseudo("Pseudo$i");
+            $participant->setMotPasse("mdp");
+
+
             $isAdmin = (bool)random_int(0, 1);
             $participant->setIsAdministrateur($isAdmin);
             $isActif = (bool)random_int(0, 1);
             $participant->setActif($isActif);
             $randomCampus = $campus[array_rand($campus)];
             $participant->setCampus($randomCampus);
-            $manager->persist($participant);
+            $motdepasse = $participant->getMotPasse();
+
+            $hashedPassword = $this->passwordHasher->hashPassword($participant, $motdepasse);
+            $participant->setMotPasse($hashedPassword);
             $participants[] = $participant;
+            $manager->persist($participant);
         }
+
         $manager->flush();
 
 
         for ($i = 1; $i <= 6; $i++) {
             $sortie = new Sortie();
-            $sortie->setNom("Sortie $i");
+            $sortie->setNom("Sortie$i");
             $dateHeureDebut = new \DateTime();
             $dateHeureDebut->modify("+$i days");
             $sortie->setDateHeureDebut($dateHeureDebut);
@@ -50,7 +67,7 @@ class SortieParticipantFixtures extends Fixture implements DependentFixtureInter
             $sortie->setDateLimiteInscription($dateLimiteInscription);
             $sortie->setDuree($i + 2);
             $sortie->setNbInscriptionsMax($i + 10);
-            $sortie->setInfosSortie("info $i");
+            $sortie->setInfosSortie("info$i");
             $randomCampus = $campus[array_rand($campus)];
             $sortie->setCampus($randomCampus);
             $randomEtat = $etat[array_rand($etat)];
